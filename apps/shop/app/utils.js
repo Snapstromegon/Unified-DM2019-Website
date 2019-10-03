7c520945af557f59896bd619d23de237ab1b7411
Raphael Höser <raphael@hoeser.info>
2019-10-03 14:34:16 +0200

    update redirect path

--- apps/shop/app/utils.js
+++ apps/shop/app/utils.js
@@ -2,7 +2,7 @@ function requireLogin(req, res, next){
   if(req.isAuthenticated()){
     next();
   } else {
-    res.redirect(`${config.loginUrl}/?redirectBack=${req.originalUrl}`);
+    res.redirect(`${config.loginUrl}/?redirectBack=${config.login.url}/${req.originalUrl}`);
   }
 }
 
