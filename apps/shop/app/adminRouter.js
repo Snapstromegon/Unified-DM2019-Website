14ee2059ac380118b65a8c196ed1f78e9504034d
Raphael Höser <raphael@hoeser.info>
2019-10-03 15:12:32 +0200

    add order deletion

--- apps/shop/app/adminRouter.js
+++ apps/shop/app/adminRouter.js
@@ -5,13 +5,23 @@ const { requireRole,requireOneOfRoles } = require('./utils.js');
 const {
   User,
   Registrant,
-  ShopOrder
+  ShopOrder,
+  ShopOrderItemSelection
 } = require('../../../models/index.js');
 
 /* GET user profile. */
 router.use('/payment', requireRole('Payment'), require('./admin/paymentRouter.js'));
 
 router.use('/handout', requireRole('Handout'), require('./admin/handoutRouter.js'));
+
+router.use('/orders/delete/:id', requireRole('OrderManager'), async (req, res) => {
+  const order = await ShopOrder.findByPk(parseInt(req.params.id), {include: [ShopOrderItemSelection]});
+  for(const selection of order.ShopOrderItemSelections){
+    await selection.destroy();
+  }
+  await order.destroy();
+  res.redirect('/admin/listAll');
+});
 
 router.get('/listAll', requireOneOfRoles('Payment', 'Handout'), async (req, res) => {
   const users = await User.findAll({
