c23cddea5aa68acc36ad08bda3797cdc50db94fe
Raphael Höser <raphael@hoeser.info>
2019-10-09 17:07:38 +0200

    add role summary to allow listall

--- apps/shop/app/adminRouter.js
+++ apps/shop/app/adminRouter.js
@@ -10,38 +10,56 @@ const {
 } = require('../../../models/index.js');
 
 /* GET user profile. */
-router.use('/payment', requireRole('Payment'), require('./admin/paymentRouter.js'));
+router.use(
+  '/payment',
+  requireRole('Payment'),
+  require('./admin/paymentRouter.js')
+);
 
-router.use('/handout', requireRole('Handout'), require('./admin/handoutRouter.js'));
+router.use(
+  '/handout',
+  requireRole('Handout'),
+  require('./admin/handoutRouter.js')
+);
 
-router.use('/orders/delete/:id', requireRole('OrderManager'), async (req, res) => {
-  const order = await ShopOrder.findByPk(parseInt(req.params.id), {include: [ShopOrderItemSelection]});
-  for(const selection of order.ShopOrderItemSelections){
-    await selection.destroy();
-  }
-  await order.destroy();
-  res.redirect('/admin/listAll');
-});
-
-router.get('/listAll', requireRole('Payment', 'Handout'), async (req, res) => {
-  const users = await User.findAll({
-    subQuery: false,
-    include: [
-      {
-        model: ShopOrder,
-        required: true
-      },
-      Registrant
-    ]
-  });
-  for (const user of users) {
-    user.orders = await ShopOrder.findAllIncludingItems({
-      where: {
-        UserId: user.id
-      }
+router.use(
+  '/orders/delete/:id',
+  requireRole('OrderManager'),
+  async (req, res) => {
+    const order = await ShopOrder.findByPk(parseInt(req.params.id), {
+      include: [ShopOrderItemSelection]
     });
+    for (const selection of order.ShopOrderItemSelections) {
+      await selection.destroy();
+    }
+    await order.destroy();
+    res.redirect('/admin/listAll');
   }
-  res.render('pages/admin/listAll.njk', { req, users });
-});
+);
+
+router.get(
+  '/listAll',
+  requireRole('Payment', 'Handout', 'Summary'),
+  async (req, res) => {
+    const users = await User.findAll({
+      subQuery: false,
+      include: [
+        {
+          model: ShopOrder,
+          required: true
+        },
+        Registrant
+      ]
+    });
+    for (const user of users) {
+      user.orders = await ShopOrder.findAllIncludingItems({
+        where: {
+          UserId: user.id
+        }
+      });
+    }
+    res.render('pages/admin/listAll.njk', { req, users });
+  }
+);
 
 module.exports = router;
