107956f10c32e745781fa55366047df88dbdb629
Raphael Höser <raphael@hoeser.info>
2019-10-30 17:33:31 +0100

    disable shop

--- apps/shop/app/shopRouter.js
+++ apps/shop/app/shopRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { requireLogin } = require('./utils.js');
+const { requireLogin, requireRole } = require('./utils.js');
 
 const router = express.Router();
 const {
