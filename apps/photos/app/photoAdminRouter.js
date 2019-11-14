e42d28b51c9a37eb192d3e5e776b8b11c7b0b444
Raphael Höser <raphael@hoeser.info>
2019-11-14 17:19:10 +0100

    photos cleaning

--- apps/photos/app/photoAdminRouter.js
+++ apps/photos/app/photoAdminRouter.js
@@ -1,20 +1,22 @@
-const express = require("express");
-const multer = require("multer");
-const sharp = require("sharp");
-const path = require("path");
-const upload = multer({ dest: path.join(__dirname, "..", "uploads") });
+const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
+const fsP = fs.promises;
+const upload = multer({ dest: path.join(__dirname, '..', 'uploads') });
 
 const router = express.Router();
 
-router.post("/upload", upload.array("photos"), async (req, res, next) => {
+router.post('/upload', upload.array('photos'), async (req, res, next) => {
   for (const file of req.files) {
-    const fileName = path.parse(file.originalname).name;
+    const fileName = req.body.tag + '__' + path.parse(file.originalname).name;
     const input = sharp(file.path)
       .resize(1920)
       .composite([
         {
-          input: path.join(__dirname, "../static/res/img/image_overlay.png"),
-          gravity: "southeast"
+          input: path.join(__dirname, '../static/res/img/image_overlay.png'),
+          gravity: 'southeast'
         }
       ]);
     const inputClone = input.clone();
@@ -22,12 +24,47 @@ router.post("/upload", upload.array("photos"), async (req, res, next) => {
     await inputClone
       .jpeg({ quality: 50 })
       .toFile(`static/photos/${fileName}.jpg`);
+    await fsP.unlink(file.path);
   }
   next();
 });
 
-router.all("/upload", async (req, res) => {
-  res.render("pages/admin/upload.njk", { req });
+router.all('/upload', upload.array('photos'), async (req, res) => {
+  res.render('pages/admin/upload.njk', { req });
 });
 
+router.get('/clean/:tag', upload.array('photos'), async (req, res) => {
+  if (req.params.tag == 'delete_all') {
+    const images = (
+      await fsP.readdir(path.join(__dirname, '..', 'static', 'photos'))
+    ).filter(f => f != '.gitkeep');
+    for (const file of images) {
+      await fsP.unlink(path.join(__dirname, '..', 'static', 'photos', file));
+    }
+    const uploads = (
+      await fsP.readdir(path.join(__dirname, '..', 'uploads'))
+    ).filter(f => f != '.gitkeep');
+    for (const file of uploads) {
+      await fsP.unlink(path.join(__dirname, '..', 'uploads', file));
+    }
+  } else {
+    const images = (
+      await fsP.readdir(path.join(__dirname, '..', 'static', 'photos'))
+    )
+      .filter(f => f != '.gitkeep')
+      .filter(f => f.startsWith(req.params.tag + '__'));
+    for (const file of images) {
+      await fsP.unlink(path.join(__dirname, '..', 'static', 'photos', file));
+    }
+  }
+  res.redirect('/all');
+});
+
+router.get('/clean', upload.array('photos'), async (req, res) => {
+  const images = (
+    await fsP.readdir(path.join(__dirname, '..', 'static', 'photos'))
+  ).filter(f => f != '.gitkeep');
+  const tags = Array.from(new Set(images.map(i => i.split('__')[0])));
+  res.render('pages/admin/clean.njk', { req, tags });
+});
 module.exports = router;
--- apps/photos/app/photoRouter.js
+++ apps/photos/app/photoRouter.js
@@ -1,13 +1,22 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 const fsP = fs.promises;
-const path = require("path");
+const path = require('path');
 
 const router = express.Router();
 
-async function getAllImages({ format = ".webp" }) {
+function shuffleArray(input) {
+  const array = Array.from(input);
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+async function getAllImages({ format = '.webp' }) {
   const files = await fsP.readdir(
-    path.join(__dirname, "..", "static", "photos")
+    path.join(__dirname, '..', 'static', 'photos')
   );
   const matching = files
     .filter(f => path.parse(f).ext == format)
@@ -16,24 +25,32 @@ async function getAllImages({ format = ".webp" }) {
   return matching;
 }
 
-router.get("/random", async (req, res) => {
-  const images = await getAllImages({ format: req.query.format || ".webp" });
+router.get('/random', async (req, res) => {
+  const images = await getAllImages({ format: req.query.format || '.webp' });
   res.redirect(
     `/photos/${images[Math.floor(Math.random() * images.length)]}.webp`
   );
 });
 
+router.get('/random/json', async (req, res) => {
+  const images = await getAllImages({ format: req.query.format || '.webp' });
+  const shuffled = shuffleArray(images);
+  res.json(shuffled);
+});
+
 router.get('/all/json', async (req, res) => {
-  const images = await getAllImages({ format: req.query.format || ".webp" });
+  const images = await getAllImages({ format: req.query.format || '.webp' });
   res.json(images.map(i => `/photos/${i}.webp`));
 });
 
-router.get("/all", async (req, res) => {
+router.get('/all', async (req, res) => {
   const page = parseInt(req.query.page) || 0;
   const perPage = parseInt(req.query.perPage) || 16;
   const start = page * perPage;
-  const images = (await getAllImages({ format: req.query.format || ".webp" })).slice(start, start + perPage);
-  res.render("pages/all.njk", {
+  const images = (
+    await getAllImages({ format: req.query.format || '.webp' })
+  ).slice(start, start + perPage);
+  res.render('pages/all.njk', {
     req,
     images: images,
     page: page,
